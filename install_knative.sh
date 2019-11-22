606ec83bed634b97a773eb8b82bd95beef7a2760
Johanna Saladas <johannasaladas@johannas-mbp.hursley.uk.ibm.com>
2019-11-22 15:20:04 +0000

    Added bash script for installing Knative

---
+++ install_knative.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+set -e
+
+
+install_knative_k8s() {
+   
+   kubectl apply --selector knative.dev/crd-install=true \
+   --filename https://github.com/knative/serving/releases/download/v0.10.0/serving.yaml \
+   --filename https://github.com/knative/eventing/releases/download/v0.10.0/release.yaml \
+   --filename https://github.com/knative/serving/releases/download/v0.10.0/monitoring.yaml
+
+}
+
+install_knative_eventing_k8s() {
+   kubectl apply --filename https://github.com/knative/serving/releases/download/v0.10.0/serving.yaml \
+   --filename https://github.com/knative/eventing/releases/download/v0.10.0/release.yaml \
+   --filename https://github.com/knative/serving/releases/download/v0.10.0/monitoring.yaml
+
+}
+
+
+setup_knative() {
+    # Install Knative
+    install_knative_k8s 
+
+    # Install eventing
+    install_knative_eventing_k8s
+}
+
+show_knative_status(){
+   kubectl get pods --namespace knative-serving
+   kubectl get pods --namespace knative-eventing
+   kubectl get pods --namespace knative-monitoring
+}
+
+setup_istio
+show_knative_status
